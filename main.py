5af440e966549f71956ef9a62388d09e23830c9d
Rongtian Guo <rongtian.guo@gmail.com>
2023-03-26 22:07:10 +1030

    Refactor ship positioning functions
    This commit refactors the ship positioning functions to improve readability and modularity. The get_ship_position function now takes the ship length, placement, and direction extension as parameters to create a single ship's position. The get_ship_positions function iterates through the provided ship types, assigns random placement and direction extension, and calls the get_ship_position function to generate the positions for each ship. The refactored code makes it easier to understand and maintain.

--- main.py
+++ main.py
@@ -5,42 +5,45 @@
 # Due date is 26/03/2023
 # I agree to the academic integrity policy and will complete the assignment in accordance with its terms.
 
+
 import random
-# board_vertically=range(1,11)
-# board_horizontally=range(1,11)
-# board_size=[board_vertically,board_horizontally]
-# ship_carrier=[(1,5),(5,1)]
-# ship_battleship=[(1,4),(4,1)]
-# ship_destroyer=(random.choice(1,2))
-# if ship_destroyer[0] == 1:
-#     pass
 
-def get_ship_position(ship_type,ship_lenght):
-    place_ship=random.choice(["vertically","horizontally"])
-    direction_extend_ship=random.choice(["-","+"])
-    if ship_type == "destoryer":
-        position_destroyer=[]
-        position_head_ship=random.randint(1,10),random.randint(1,10)
-        position_destroyer.append(position_head_ship)
-        if place_ship == "vertically":
-            for lenght_extend in range(1,ship_lenght):
-                position_destroyer.append((eval(f"{position_head_ship[0]}{direction_extend_ship}{lenght_extend}"),position_head_ship[1]))
-        else:
-            for lenght_extend in range(1,ship_lenght):
-                position_destroyer.append((position_head_ship[0],eval(f"{position_head_ship[1]}{direction_extend_ship}{lenght_extend}")))
-                
-    
-        return position_destroyer
-    
-ship_ready=get_ship_position("destoryer",2)
+def get_ship_position(ship_lenght, place_ship, direction_extend_ship):
+    position_ship = []
+    position_head_ship = random.randint(1, 3), random.randint(1, 3)
+    position_ship.append(position_head_ship)
+
+    if place_ship=="vertically":
+        for lenght_extend in range(1, ship_lenght):
+            position_ship.append((eval(f"{position_head_ship[0]}{direction_extend_ship}{lenght_extend}"), position_head_ship[1]))
+    else:
+        for lenght_extend in range(1, ship_lenght):
+            position_ship.append((position_head_ship[0], eval(f"{position_head_ship[1]}{direction_extend_ship}{lenght_extend}")))
+
+    return position_ship
+
+def get_ship_positions(ship_types):
+    all_ship_positions = []
+
+    for ship in ship_types:
+        place_ship = random.choice(["vertically", "horizontally"])
+        direction_extend_ship = random.choice(["-", "+"])
+
+        if ship == "destroyer":
+            all_ship_positions.append(get_ship_position(2, place_ship, direction_extend_ship))
+        elif ship == "submarine":
+            all_ship_positions.append(get_ship_position(3, place_ship, direction_extend_ship))
+        elif ship == "cruiser":
+            all_ship_positions.append(get_ship_position(3, place_ship, direction_extend_ship))
+        elif ship == "battleship":
+            all_ship_positions.append(get_ship_position(4, place_ship, direction_extend_ship))
+        elif ship == "carrier":
+            all_ship_positions.append(get_ship_position(5, place_ship, direction_extend_ship))
+
+    return all_ship_positions
+
+ship_ready = get_ship_positions(["carrier", "battleship", "cruiser", "submarine", "destroyer"])
 print(ship_ready)
-
-# save= "+"
-# print(eval(f"1{save}2"))
-
-# print(random.choice(board_size[0]),random.choice(board_size[1]))
-
-# print(random.choice(ship_carrier))
 
 # history=[]
 
