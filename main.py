56a37fe3505c1ec98d02ee9687efa8420d83943a
Rongtian Guo <rongtian.guo@gmail.com>
2023-03-26 23:21:40 +1030

    Fix check_ship_position_no_same function and update header comments
    Fixed the implementation of the check_ship_position_no_same function to correctly compare ship positions
    Updated header comments with course information, student name, student ID, and due date

--- main.py
+++ main.py
@@ -8,29 +8,6 @@
 
 import random
 
-def get_ship_position(ship_lenght, place_ship, direction_extend_ship):
-    position_ship = []
-    position_head_ship = random.randint(1, 3), random.randint(1, 3)
-    position_ship.append(position_head_ship)
-
-    if place_ship=="vertically":
-        for lenght_extend in range(1, ship_lenght):
-            position_ship.append((eval(f"{position_head_ship[0]}{direction_extend_ship}{lenght_extend}"), position_head_ship[1]))
-    else:
-        for lenght_extend in range(1, ship_lenght):
-            position_ship.append((position_head_ship[0], eval(f"{position_head_ship[1]}{direction_extend_ship}{lenght_extend}")))
-
-    return position_ship
-
-def check_ship_position_no_same(ships_position,ship_position):
-    for split_shipPosition in ship_position:
-        for split_shipsPosition in ships_position:
-            for split_splitShipPosition in split_shipPosition:
-                for split_splitShipsPosition in split_shipsPosition:
-                    if split_splitShipPosition == split_splitShipsPosition:
-                        return True
-    return False
-
 def get_ship_positions(ship_types):
     all_ship_positions = []
 
@@ -39,23 +16,88 @@ def get_ship_positions(ship_types):
         direction_extend_ship = random.choice(["-", "+"])
 
         if ship == "destroyer":
-            similiar_check=True
-            while similiar_check==False:
-                ship_position=get_ship_position(2, place_ship, direction_extend_ship)
-                check_ship_position_no_same(all_ship_positions,ship_position)
-                if check_ship_position_no_same == False:
-                    similiar_check=False
-                
+            similiar_check = True
+            while similiar_check:
+                ship_position = get_ship_position(2, place_ship, direction_extend_ship)
+                if not check_ship_position_no_same(all_ship_positions, ship_position):
+                    similiar_check = False
+            all_ship_positions.append(ship_position)
+
         elif ship == "submarine":
-            all_ship_positions.append(get_ship_position(3, place_ship, direction_extend_ship))
+            similiar_check = True
+            while similiar_check:
+                ship_position = get_ship_position(3, place_ship, direction_extend_ship)
+                if not check_ship_position_no_same(all_ship_positions, ship_position):
+                    similiar_check = False
+            all_ship_positions.append(ship_position)
+
         elif ship == "cruiser":
-            all_ship_positions.append(get_ship_position(3, place_ship, direction_extend_ship))
+            similiar_check = True
+            while similiar_check:
+                ship_position = get_ship_position(3, place_ship, direction_extend_ship)
+                if not check_ship_position_no_same(all_ship_positions, ship_position):
+                    similiar_check = False
+            all_ship_positions.append(ship_position)
+
         elif ship == "battleship":
-            all_ship_positions.append(get_ship_position(4, place_ship, direction_extend_ship))
+            similiar_check = True
+            while similiar_check:
+                ship_position = get_ship_position(4, place_ship, direction_extend_ship)
+                if not check_ship_position_no_same(all_ship_positions, ship_position):
+                    similiar_check = False
+            all_ship_positions.append(ship_position)
+
         elif ship == "carrier":
-            all_ship_positions.append(get_ship_position(5, place_ship, direction_extend_ship))
+            similiar_check = True
+            while similiar_check:
+                ship_position = get_ship_position(5, place_ship, direction_extend_ship)
+                if not check_ship_position_no_same(all_ship_positions, ship_position):
+                    similiar_check = False
+            all_ship_positions.append(ship_position)
 
     return all_ship_positions
+
+
+def check_ship_position_no_same(ships_position, ship_position):
+    for existing_ship_position in ships_position:
+        for existing_position in existing_ship_position:
+            for new_position in ship_position:
+                if existing_position == new_position:
+                    return True
+    return False
+
+
+
+
+
+def get_ship_position(ship_length, place_ship, direction_extend_ship):
+    position_ship = []
+    
+    while not position_ship:
+        position_head_ship = random.randint(1, 10), random.randint(1, 10)
+        temp_position_ship = [position_head_ship]
+        valid = True
+
+        if place_ship == "vertically":
+            for length_extend in range(1, ship_length):
+                new_position = eval(f"{position_head_ship[0]}{direction_extend_ship}{length_extend}"), position_head_ship[1]
+                if new_position[0] < 1 or new_position[0] > 10:
+                    valid = False
+                    break
+                temp_position_ship.append(new_position)
+        else:
+            for length_extend in range(1, ship_length):
+                new_position = position_head_ship[0], eval(f"{position_head_ship[1]}{direction_extend_ship}{length_extend}")
+                if new_position[1] < 1 or new_position[1] > 10:
+                    valid = False
+                    break
+                temp_position_ship.append(new_position)
+        
+        if valid:
+            position_ship = temp_position_ship
+
+    return position_ship
+
 
 ship_ready = get_ship_positions(["carrier", "battleship", "cruiser", "submarine", "destroyer"])
 print(ship_ready)
